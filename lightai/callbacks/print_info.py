72067f0d85af69c1b8a1832dda82186cf1d90a93
Apollo <sushi90817564@gmail.com>
2018-11-07 19:48:55 +0800

    fix bug

--- lightai/callbacks/print_info.py
+++ lightai/callbacks/print_info.py
@@ -4,7 +4,7 @@ from ..callback import *
 
 class Printer(Callback):
     def __init__(self, metrics):
-        self.layout = '{:^11}' + '{:11.6f}' * (3 + len(metrics))
+        self.layout = '{:^11}' + '{:11.6f}' * (2 + len(metrics))
         self.metrics = metrics
 
     def on_train_begin(self, **kwargs):
@@ -16,4 +16,4 @@ class Printer(Callback):
 
     def on_epoch_end(self, trn_loss: float, eval_res: List[float], epoch: int,
                      bar, **kwargs):
-        bar.write(self.layout.format(epoch, trn_loss, *eval_res, elapsed_time))
+        bar.write(self.layout.format(epoch, trn_loss, *eval_res))
