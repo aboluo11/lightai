884933f4bc9e0db71a6db6bbfe27f41e6204f961
Apollo <sushi90817564@gmail.com>
2018-11-17 14:55:52 +0800

    fix bug

--- lightai/core.py
+++ lightai/core.py
@@ -25,7 +25,7 @@ import torch
 from torch import nn, Tensor, optim
 import torch.nn.functional as F
 import torchvision
-from torch.utils.data import BatchSampler, RandomSampler, Sampler, SequentialSampler, DataLoader
+from torch.utils.data import BatchSampler, RandomSampler, Sampler, SequentialSampler, DataLoader, SubsetRandomSampler
 
 
 def T(x: np.ndarray, cuda=True):
